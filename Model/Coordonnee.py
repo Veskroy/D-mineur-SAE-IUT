0e3ddea86a3571a55f16e8d81d9c0099ba62b0c0
Vice Kpat <vincentkpatinde14@gmail.com>
2023-01-09 21:40:17 +0100

    Coordonnée implémentées test OK

--- Model/Coordonnee.py
+++ Model/Coordonnee.py
@@ -24,17 +24,18 @@ def type_coordonnee(coord: tuple) -> bool:
 def construireCoordonnee(n_ligne: int ,n_col:int):
     if not (type(n_col)== int and type(n_ligne)==int):
         raise TypeError(f"construireCoordonnee : Le numéro de ligne {type(n_ligne)} ou le numéro de colonne {type(n_col)} ne sont pas des entiers")
-    if not n_ligne > 0 and n_col > 0:
+    if not n_ligne >= 0 and n_col >= 0:
         raise ValueError(f"construireCoordonnee : Le numéro de ligne {n_ligne} ou de colonne {n_col} sont pas positifs")
     return (n_ligne,n_col)
 
 # ajout de la fonction getligneCoordonnee
 
-def getligneCoordonnee(coordonner_tuple):
-    if not (len(coordonner_tuple)== 2 and (coordonner_tuple[1]== int and coordonner_tuple[2]==int)):
-        raise ValueError ("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
+def getLigneCoordonnee(coordonner_tuple):
+    if  not (len(coordonner_tuple)== 2 and  type(coordonner_tuple) == tuple and (type(coordonner_tuple[0])== int and type(coordonner_tuple[1])==int)):
+        raise TypeError ("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
     return coordonner_tuple[0]
+# ajout de la fonction getColonneCoordonnee
 def getColonneCoordonnee(coordonner_tuple):
-    if not (len(coordonner_tuple)== 2 and (coordonner_tuple[1]== int and coordonner_tuple[2]==int)):
-        raise ValueError ("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
+    if not (len(coordonner_tuple)== 2 and type(coordonner_tuple) == tuple and  (type(coordonner_tuple[0]) == int and type(coordonner_tuple[1])==int)):
+        raise TypeError ("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
     return coordonner_tuple[1]
