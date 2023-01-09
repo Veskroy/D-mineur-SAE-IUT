543a0a3ee4c621fe348564bf5d81428bf851d4e4
Vice Kpat <vincentkpatinde14@gmail.com>
2023-01-09 20:47:59 +0100

    pycharm

--- Model/Coordonnee.py
+++ Model/Coordonnee.py
@@ -19,3 +19,22 @@ def type_coordonnee(coord: tuple) -> bool:
         and coord[0] >= 0 and coord[1] >= 0
 
 
+# Ajout de la fonction construireCoordonner
+
+def construireCoordonnee(n_ligne: int ,n_col:int):
+    if not (type(n_col)== int and type(n_ligne)==int):
+        raise TypeError(f"construireCoordonnee : Le numéro de ligne {type(n_ligne)} ou le numéro de colonne {type(n_col)} ne sont pas des entiers")
+    if not n_ligne > 0 and n_col > 0:
+        raise ValueError(f"construireCoordonnee : Le numéro de ligne {n_ligne} ou de colonne {n_col} sont pas positifs")
+    return (n_ligne,n_col)
+
+# ajout de la fonction getligneCoordonnee
+
+def getligneCoordonnee(coordonner_tuple):
+    if not (len(coordonner_tuple)== 2 and (coordonner_tuple[1]== int and coordonner_tuple[2]==int)):
+        raise ValueError ("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
+    return coordonner_tuple[0]
+def getColonneCoordonnee(coordonner_tuple):
+    if not (len(coordonner_tuple)== 2 and (coordonner_tuple[1]== int and coordonner_tuple[2]==int)):
+        raise ValueError ("getLigneCoordonnee : Le paramètre n’est pas une coordonnée")
+    return coordonner_tuple[1]
